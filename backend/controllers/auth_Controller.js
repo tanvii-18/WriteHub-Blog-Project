bd4c4f832bb7b732578c606509baf24de2b2d59c
tanvii-18 <8438tanvipatel@gmail.com>
2025-12-30 21:30:27 +0530

    update 6 files

---
+++ backend/controllers/auth_Controller.js
@@ -0,0 +1,22 @@
+import { users } from "../model/authModel";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = users.findOne({ email });
+
+    if (!existingUser) {
+      res.json({ message: "user already registered!" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await users.create({ name, email, password: hashedPassword });
+
+    res.json({ message: "user registered!", user });
+  } catch (error) {
+    res.json({ message: "user can't register!", error });
+  }
+};
