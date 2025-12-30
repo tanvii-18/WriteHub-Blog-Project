45d016108d44da1f4c487486c4a7c5c668c4430f
tanvii-18 <8438tanvipatel@gmail.com>
2025-12-30 23:21:19 +0530

    30-12-25

--- backend/controllers/auth_Controller.js
+++ backend/controllers/auth_Controller.js
@@ -1,14 +1,15 @@
 import { users } from "../model/authModel.js";
 import bcrypt from "bcrypt";
 
+// user registration
 export const signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
     const existingUser = users.findOne({ email });
 
-    if (!existingUser) {
-      res.json({ message: "user already registered!" });
+    if (existingUser) {
+      return res.json({ message: "user already registered!" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -20,3 +21,25 @@ export const signup = async (req, res) => {
     res.json({ message: "user can't register!", error });
   }
 };
+
+// User Login
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await users.findOne({ email: email });
+
+  if (!user) {
+    return res.json({ message: "user not found!" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.json({ message: "wrong password!" });
+  }
+
+  res.json({ message: "Login Successfully!!", user });
+};
+
+// logout
+export const logout = (req, res) => {};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
-import { signup } from "../controllers/auth_Controller.js";
+import { login, signup } from "../controllers/auth_Controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
