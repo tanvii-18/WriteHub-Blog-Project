fe08b1dbabe8a1f1ae0cff0fb69d8ccd18f78770
tanvii-18 <8438tanvipatel@gmail.com>
2025-12-31 21:35:41 +0530

    update auth_Controller.js

--- backend/controllers/auth_Controller.js
+++ backend/controllers/auth_Controller.js
@@ -2,7 +2,7 @@ import { users } from "../model/authModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "blogsKey";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // user registration
 export const signup = async (req, res) => {
